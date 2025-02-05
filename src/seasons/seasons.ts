526e948a92857d66ea2c6d3c7583f7de198cd7e0
Rafa <rafacv23@gmail.com>
2025-02-05 14:33:59 +0100

    add pagination support to getSeasons method and update tests for new functionality

--- src/seasons/seasons.ts
+++ src/seasons/seasons.ts
@@ -4,7 +4,21 @@ import { Season } from "./types"
 const resourceName = "seasons"
 
 export class Seasons extends Base {
-  getSeasons(): Promise<Season[]> {
-    return this.request(`/${resourceName}`)
+  getSeasons({
+    limit,
+    offset,
+  }: { limit?: number; offset?: number } = {}): Promise<Season[]> {
+    // Crear un objeto para los parámetros de consulta
+    const params: Record<string, string> = {}
+
+    // Agregar los parámetros solo si están definidos
+    if (limit !== undefined) params.limit = String(limit)
+    if (offset !== undefined) params.offset = String(offset)
+
+    // Crear la URL con parámetros de consulta usando URLSearchParams
+    const queryString = new URLSearchParams(params).toString()
+    const url = `/${resourceName}?${queryString}`
+
+    return this.request(url)
   }
 }
--- tests/api.test.ts
+++ tests/api.test.ts
@@ -3,8 +3,8 @@ import F1Api from "../src"
 
 const f1Api = new F1Api()
 
-describe("F1Api", () => {
-  it("debería obtener datos correctamente", async () => {
+describe("Seasons endpoints", () => {
+  it("returns all the f1 seasons", async () => {
     const data = await f1Api.getSeasons()
     expect(data).toEqual(
       expect.objectContaining({
@@ -21,6 +21,28 @@ describe("F1Api", () => {
         offset: expect.any(Number),
         total: expect.any(Number),
         url: expect.any(String),
+      })
+    )
+  })
+
+  it("returns f1 seasons with limit and offset", async () => {
+    const data = await f1Api.getSeasons({ limit: 1, offset: 1 })
+    console.log(data)
+    expect(data).toEqual(
+      expect.objectContaining({
+        api: expect.any(String),
+        url: expect.any(String),
+        limit: 1,
+        offset: 1,
+        total: expect.any(Number),
+        championships: expect.arrayContaining([
+          expect.objectContaining({
+            championshipId: expect.any(String),
+            championshipName: expect.any(String),
+            url: expect.any(String),
+            year: expect.any(Number),
+          }),
+        ]),
       })
     )
   })
