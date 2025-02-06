53e36b566dbfe554e32ee918caa232b38ab1bdb5
Rafa <rafacv23@gmail.com>
2025-02-06 11:37:39 +0100

    add Circuit interface for circuit data representation

---
+++ src/circuits/types.ts
@@ -0,0 +1,14 @@
+export interface Circuit {
+  circuitId: string
+  circuitName: string
+  country: string
+  city: string
+  circuitLength: number
+  firstParticipationYear: number
+  numberOfCorners: number
+  lapRecord: string
+  fastestLapDriverId: string
+  fastestLapTeamId: string
+  fastestLapYear: number
+  url: string
+}
