26814c91764a241c2b728d12d5d5b488676593a5
Rafa <rafacv23@gmail.com>
2025-02-06 11:37:28 +0100

    extend F1Api interface to include Circuits functionality

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { Base } from "./base"
+import { Circuits } from "./circuits/circuits"
 import { Seasons } from "./seasons/seasons"
 import { applyMixins } from "./utils"
 
 class F1Api extends Base {}
-interface F1Api extends Seasons {}
+interface F1Api extends Seasons, Circuits {}
 
-applyMixins(F1Api, [Seasons])
+applyMixins(F1Api, [Seasons, Circuits])
 
 export default F1Api
