0c05e5be7f9a125e7e72986cd09d820b21d29a7f
Rafa <rafacv23@gmail.com>
2025-02-06 12:09:33 +0100

    add Standings integration to F1Api interface

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { Base } from "./base"
 import { Circuits } from "./circuits/circuits"
 import { Seasons } from "./seasons/seasons"
+import { Standings } from "./standings/standings"
 import { applyMixins } from "./utils"
 
 class F1Api extends Base {}
-interface F1Api extends Seasons, Circuits {}
+interface F1Api extends Seasons, Circuits, Standings {}
 
-applyMixins(F1Api, [Seasons, Circuits])
+applyMixins(F1Api, [Seasons, Circuits, Standings])
 
 export default F1Api
