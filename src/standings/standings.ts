1eac3ae0c6649f021006d8549e354b444367c8b7
Rafa <rafacv23@gmail.com>
2025-02-06 12:08:02 +0100

    add Standings class for managing driver and constructor standings retrieval

---
+++ src/standings/standings.ts
@@ -0,0 +1,64 @@
+import { Base } from "src/base"
+import { buildQueryParams } from "src/utils"
+import { ConstructorStandings, DriverStandings } from "./types"
+
+const driverStandings = "drivers-championship"
+const constructorStandings = "constructors-championship"
+
+export class Standings extends Base {
+  getDriverStandings({
+    year,
+    limit,
+    offset,
+  }: {
+    year: number
+    limit?: number
+    offset?: number
+  }): Promise<DriverStandings[]> {
+    const queryParams = buildQueryParams(limit, offset)
+
+    const url = `/${year}/${driverStandings}?${queryParams}`
+
+    return this.request(url)
+  }
+
+  getCurrentDriverStandings({
+    limit,
+    offset,
+  }: { limit?: number; offset?: number } = {}): Promise<DriverStandings[]> {
+    const queryParams = buildQueryParams(limit, offset)
+
+    const url = `/current/${driverStandings}?${queryParams}`
+
+    return this.request(url)
+  }
+
+  getConstructorStandings({
+    year,
+    limit,
+    offset,
+  }: {
+    year: number
+    limit?: number
+    offset?: number
+  }): Promise<ConstructorStandings[]> {
+    const queryParams = buildQueryParams(limit, offset)
+
+    const url = `/${year}/${constructorStandings}?${queryParams}`
+
+    return this.request(url)
+  }
+
+  getCurrentConstructorStandings({
+    limit,
+    offset,
+  }: { limit?: number; offset?: number } = {}): Promise<
+    ConstructorStandings[]
+  > {
+    const queryParams = buildQueryParams(limit, offset)
+
+    const url = `/current/${constructorStandings}?${queryParams}`
+
+    return this.request(url)
+  }
+}
