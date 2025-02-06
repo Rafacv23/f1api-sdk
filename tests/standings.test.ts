4cad4d230233d05dd8fa82a9186cb8b4d090dce5
Rafa <rafacv23@gmail.com>
2025-02-06 12:20:58 +0100

    add test for retrieving driver standings with limit and offset

--- tests/standings.test.ts
+++ tests/standings.test.ts
@@ -45,5 +45,51 @@ describe("Standings endpoints", () => {
         ]),
       })
     )
-  })
+  }),
+    it("returns driver standings for a specific year with limit and offset", async () => {
+      const data = await f1Api.getDriverStandings({
+        year: 2022,
+        limit: 1,
+        offset: 1,
+      })
+      expect(data).toEqual(
+        expect.objectContaining({
+          api: expect.any(String),
+          limit: 1,
+          offset: 1,
+          total: expect.any(Number),
+          season: expect.any(String),
+          championshipId: expect.any(String),
+          url: expect.any(String),
+          drivers_championship: expect.arrayContaining([
+            expect.objectContaining({
+              classificationId: expect.any(Number),
+              driverId: expect.any(String),
+              teamId: expect.any(String),
+              points: expect.any(Number),
+              position: expect.any(Number),
+              wins: expect.any(Number),
+              driver: expect.objectContaining({
+                name: expect.any(String),
+                surname: expect.any(String),
+                nationality: expect.any(String),
+                birthday: expect.any(String),
+                number: expect.any(Number),
+                shortName: expect.any(String),
+                url: expect.any(String),
+              }),
+              team: expect.objectContaining({
+                teamId: expect.any(String),
+                teamName: expect.any(String),
+                country: expect.any(String),
+                firstAppareance: expect.any(Number),
+                constructorsChampionships: expect.any(Number),
+                driversChampionships: expect.any(Number),
+                url: expect.any(String),
+              }),
+            }),
+          ]),
+        })
+      )
+    })
 })
