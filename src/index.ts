d6e3981bcba5338cb7872931acefacefa204455a
Rafa Canosa <rafacv23@gmail.com>
2025-02-07 16:08:47 +0100

    add Teams interface to F1Api for enhanced functionality

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import { Circuits } from "./circuits/circuits"
 import { Drivers } from "./drivers/drivers"
 import { Seasons } from "./seasons/seasons"
 import { Standings } from "./standings/standings"
+import { Teams } from "./teams/teams"
 import { applyMixins } from "./utils"
 
 class F1Api extends Base {}
-interface F1Api extends Seasons, Circuits, Standings, Drivers {}
+interface F1Api extends Seasons, Circuits, Standings, Drivers, Teams {}
 
-applyMixins(F1Api, [Seasons, Circuits, Standings, Drivers])
+applyMixins(F1Api, [Seasons, Circuits, Standings, Drivers, Teams])
 
 export default F1Api
