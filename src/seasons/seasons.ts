e32537c60d0278777372d7c22f0039dee27124d4
Rafa <rafacv23@gmail.com>
2025-04-25 20:23:44 +0200

    docs(seasons): add JSDoc comments for getSeasons method with parameters and example

--- src/seasons/seasons.ts
+++ src/seasons/seasons.ts
@@ -5,6 +5,18 @@ import { buildQueryParams } from "../utils"
 const resourceName = "seasons"
 
 export class Seasons extends Base {
+  /**
+   * getSeasons - Returns the list of the f1 seasons.
+   *
+   * @param {number} [limit] - Maximum number of results to return.
+   * @param {number} [offset] - Number of results to skip.
+   * @returns {Promise<SeasonsApiResponse>} Object array with the seasons and his info.
+   *
+   * @example
+   * ```ts
+   * const seasons = await getSeasons({ limit: 1, offset: 0 });
+   * ```
+   */
   getSeasons({
     limit,
     offset,
